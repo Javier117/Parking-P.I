fd0d71f71100c0db3707b441dc346e817446957d
Usuario <Usuario@192.168.10.107>
2016-10-25 09:17:29 -0500

    get y set bicicleta hehe

--- ParkingMVC/src/BO/bicicleta.java
+++ ParkingMVC/src/BO/bicicleta.java
@@ -12,12 +12,60 @@ package BO;
 public class bicicleta {
     String color;
     String owner;
-    String codigo_entrada;
+    String marco;
+    int hora_entrada;
+    int minutos_entrada;
     
     
-    public bicicleta()
-    {
     
+ public bicicleta(String Color , String Owner, String Marco, int Hora_Entrada, int Minutos_Entrada)
+    {       
+       this.color=Color;
+       this.owner=Owner;
+       this.marco=Marco;
+       this.hora_entrada=Hora_Entrada;
+       this.minutos_entrada=Minutos_Entrada;
+    
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getMarco() {
+        return marco;
+    }
+
+    public void setMarco(String marco) {
+        this.marco = marco;
+    }
+
+    public int getHora_entrada() {
+        return hora_entrada;
+    }
+
+    public void setHora_entrada(int hora_entrada) {
+        this.hora_entrada = hora_entrada;
+    }
+
+    public int getMinutos_entrada() {
+        return minutos_entrada;
+    }
+
+    public void setMinutos_entrada(int minutos_entrada) {
+        this.minutos_entrada = minutos_entrada;
     }
     
     
@@ -25,3 +73,4 @@ public class bicicleta {
     
     
 }
+
