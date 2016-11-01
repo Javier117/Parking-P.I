d983f54371293d96f9b7dad2cf7e208672b9afa5
Usuario <Usuario@192.168.10.89>
2016-11-01 10:11:43 -0500

    falta testear retiros

--- ParkingMVC/src/modelo/model.java
+++ ParkingMVC/src/modelo/model.java
@@ -20,7 +20,6 @@ public class model {
     Vehiculo[] ParkingMotos = new Vehiculo[45];
     Vehiculo[] ParkingCamionetas = new Vehiculo[20];
     bicicleta[] ParkingBicis = new bicicleta[20];
-
     int ingresosbicis = 0;
     int ingresosmotos = 0;
     int ingresoscarros = 0;
@@ -113,50 +112,64 @@ public class model {
         }
         return contador;
     }
-    
-    public void retirarVehiculo(int arreglo,String Placa,String Modelo,String Propietario,int hora_salida,int min_salida){
-        Vehiculo[] newParkingVehicle=ParkingCarros;
-        int legth=this.ParkingCarros.length;
-        switch(arreglo)
-        {
-            case 0:break;
-            case 1:newParkingVehicle=this.ParkingMotos;legth=this.ParkingMotos.length;break;
-            case 2:newParkingVehicle=this.ParkingCamionetas;legth=this.ParkingCamionetas.length;break;
-            default:System.out.println("Error");break;
+
+    public void retirarVehiculo(int arreglo, String Placa, String Modelo, String Propietario, int hora_salida, int min_salida) {
+        Vehiculo[] newParkingVehicle = this.ParkingCarros;
+        //Vehiculo[] Temp_array;
+        int legth = this.ParkingCarros.length;
+        switch (arreglo) {
+            case 0:
+                break;
+            case 1:
+                newParkingVehicle = this.ParkingMotos;
+                legth = this.ParkingMotos.length;
+                break;
+            case 2:
+                newParkingVehicle = this.ParkingCamionetas;
+                legth = this.ParkingCamionetas.length;
+                break;
+            default:
+                System.out.println("Error");
+                break;
         }
-    int contador = 0;
-        for (Vehiculo x :newParkingVehicle) {
-            try {
-                if (x.getPlaca().equals(Placa) && x.getOwner().equals(Propietario) && x.getModelo().equals(Modelo)) {
-                    contador += 1;
-                    System.out.println(x.getPlaca() +" modelo: "+x.getModelo()+" Retirado.");
-                    
-                    JOptionPane.showMessageDialog(null,"Hora de entrada \n"+x.getHora_entrada()+":"+x.getMinutos_entrada()+"\n"
-                               +"Hora salida"+hora_salida+":"+min_salida+"\n"+
-                            calculartarifa(x.getHora_entrada(),x.getMinutos_entrada(),hora_salida,min_salida)+" Pesos","Tarifa -"+x.getPlaca()+"- "+x.getOwner(),JOptionPane.INFORMATION_MESSAGE);
-                } else {       
-                    newParkingVehicle[contador]=x;    
-                     contador+=1;
-                    }
-                     
-                }
-             catch (Exception EX) {
-                if (contador == legth) {
-                    switch(arreglo){
-                            case 0:this.ParkingCarros=newParkingVehicle;this.ingresoscarros-=1;break;
-                            case 1:ParkingMotos=newParkingVehicle;this.ingresosmotos-=1;break;
-                            case 2:ParkingCamionetas=newParkingVehicle;this.ingresoscamionetas-=1;break;
-                            default:System.out.println("Error");break;
-                        } 
-                            break;
-                } else {
+        int contador = 0;
+        for (Vehiculo c : newParkingVehicle) {
+            if (c != null) {
+                if (Placa.equals(c.getPlaca()) && Modelo.equals(c.getModelo()) && Propietario.equals(c.getOwner())) {       
+                    System.out.println(c.getPlaca() + " modelo: " + c.getModelo() + " Retirado.");
+                    JOptionPane.showMessageDialog(null, "Hora de entrada \n" + c.getHora_entrada() + ":" + c.getMinutos_entrada() + "\n"
+                            + "Hora salida" + hora_salida + ":" + min_salida + "\n"
+                            + calculartarifa(c.getHora_entrada(), c.getMinutos_entrada(), hora_salida, min_salida)
+                            + " Pesos", "Tarifa -" + c.getPlaca() + "- " + c.getOwner(), JOptionPane.INFORMATION_MESSAGE);
+                    newParkingVehicle[contador] = null;
                     contador += 1;
                 }
+                else contador += 1;
             }
+            switch (arreglo) {
+                case 0:
+                    ParkingCarros = newParkingVehicle;
+                    this.ingresoscarros -= 1;
+                    break;
+                case 1:
+                    ParkingMotos = newParkingVehicle;
+                    this.ingresosmotos -= 1;
+                    break;
+                case 2:
+                    ParkingCamionetas = newParkingVehicle;
+                    this.ingresoscamionetas -= 1;
+                    break;
+                default:
+                    System.out.println("Error");
+                    break;
+            }
+
         }
     }
+
     
-    public void RetirarBici(String Num_Marco, String Propietario,int hora_salida,int min_salida) {
+
+    public void RetirarBici(String Num_Marco, String Propietario, int hora_salida, int min_salida) {
         bicicleta[] newParkingBicis = new bicicleta[20];
         int contador = 0;
         for (bicicleta b : ParkingBicis) {
@@ -164,10 +177,10 @@ public class model {
                 if (b.getMarco().equals(Num_Marco) && b.getOwner().equals(Propietario)) {
                     contador += 1;
                     System.out.println(b.getMarco() + " Retirada.");
-                    
-                    JOptionPane.showMessageDialog(null,"Hora de entrada \n"+b.getHora_entrada()+":"+b.getMinutos_entrada()+"\n"
-                               +"Hora salida"+hora_salida+":"+min_salida+"\n"+
-                            calculartarifa(b.getHora_entrada(),b.getMinutos_entrada(),hora_salida,min_salida)+" Pesos","Tarifa -"+b.getMarco()+"- "+b.getOwner(),JOptionPane.INFORMATION_MESSAGE);
+
+                    JOptionPane.showMessageDialog(null, "Hora de entrada \n" + b.getHora_entrada() + ":" + b.getMinutos_entrada() + "\n"
+                            + "Hora salida" + hora_salida + ":" + min_salida + "\n"
+                            + calculartarifa(b.getHora_entrada(), b.getMinutos_entrada(), hora_salida, min_salida) + " Pesos", "Tarifa -" + b.getMarco() + "- " + b.getOwner(), JOptionPane.INFORMATION_MESSAGE);
 
                 } else {
                     newParkingBicis[contador] = b;
@@ -185,26 +198,25 @@ public class model {
 
         }
 
-        this.ingresosbicis -=1;
+        this.ingresosbicis -= 1;
 
     }
 
-        private int calculartarifa(int hora_entrada,int minuto_entrada,int hora_salida,int min_salida){
+    private int calculartarifa(int hora_entrada, int minuto_entrada, int hora_salida, int min_salida) {
         // minuto a 50
-        int totalentrada=hora_entrada*60;
-        totalentrada+=minuto_entrada;
-        int total_salida=hora_salida*60;
-        total_salida+=min_salida;
-        int tarifa=totalentrada-total_salida;
-         if(tarifa<0){
-            tarifa*=-1;
+        int totalentrada = hora_entrada * 60;
+        totalentrada += minuto_entrada;
+        int total_salida = hora_salida * 60;
+        total_salida += min_salida;
+        int tarifa = totalentrada - total_salida;
+        if (tarifa < 0) {
+            tarifa *= -1;
         }
-        
-        tarifa*=50;
+
+        tarifa *= 50;
         return tarifa;
-        }
-    
-    
+    }
+
     //GETTERS y SETTERS
     public bicicleta[] getParkingBicis() {
         return ParkingBicis;
